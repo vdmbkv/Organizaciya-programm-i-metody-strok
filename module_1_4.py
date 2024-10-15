1b4c6d1bedf076a9dae592f7a0a2036c0582ac7b
vdmbkv <lpnight2@gmail.com>
2024-10-16 04:24:12 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,15 @@
+# 1
+
+my_string = input("Biysk: ") # Altayskiy Krai
+print(len(my_string))
+
+# 2
+
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
+
+# был использован метод "len" в первом задании. Брал информацию со страницы, ссылку на которую оставили в ДЗ. Надеюсь , верно понял задание.
+
